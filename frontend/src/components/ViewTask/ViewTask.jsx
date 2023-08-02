9df7ad3fed0ad1c9258c767aa328fcb07657edeb
Diego Bejar <diebejardelaguila@gmail.com>
2023-08-02 00:12:01 -0500

    feature: Add library axios

--- frontend/src/components/ViewTask/ViewTask.jsx
+++ frontend/src/components/ViewTask/ViewTask.jsx
@@ -1,11 +1,15 @@
+import Card from '../card/Card';
 import './ViewTask.css';
 
 export default function ViewTask({ title }) {
 
+    
 
     return (
         <div className="view-task">
             {title}
+            <Card />
+            <Card />
         </div>
     )
 
--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -1,10 +1,18 @@
 
+import './Card.css'
+import Button from '../button/Button'
 
 
-function Card() {
+function Card({title, description}) {
     return (
-        <div>
-            
+        <div className="card">
+            <div className="card__content">
+                <h4>{title}</h4>
+
+                <p className="card__description">{description}</p>
+                
+                <Button text="Read More" />
+            </div>
         </div>
     )
 }
