fc46980e370b5e821ecba1f3ae118b9f42fc76d0
Diego Bejar <diebejardelaguila@gmail.com>
2023-08-02 00:27:06 -0500

    feature: Add services todos

---
+++ frontend/src/services/TodoServices.js
@@ -0,0 +1,77 @@
+import api from "./ApiBase.js";
+
+
+
+const createTodo = (data) => {
+    const URL = "todos";
+  
+    return api.post(URL, data).then((response) => response?.data);
+  };
+  
+const updateTodo = (data, id) => {
+  const URL = "todos/"+id;
+
+  return api.put(URL, data).then((response) => response.data);
+};
+
+const getTodos = () => {
+  const URL = "todos";
+
+  const response = api
+    .get(URL)
+    .then((resolve) => resolve.data);
+
+  return response;
+};
+
+const getTodoById = (id) => {
+  const URL = "todos/"+id;
+
+  const response = api
+    .get(URL)
+    .then((resolve) => resolve.data);
+
+  return response;
+};
+
+const deleteTodoById = (id) => {
+  const URL = "auth/reset-password";
+
+  return api.delete(URL, id).then((response) => response.data);
+};
+
+  
+  
+const AuthService = {
+  createTodo,
+  updateTodo,
+  getTodos,
+  getTodoById,
+  deleteTodoById,
+};
+
+export default AuthService;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
