dce305eca44498cd06ac5bfcce53c1f34dd717ec
Diego Bejar <diebejardelaguila@gmail.com>
2023-08-02 16:20:36 -0500

    feature: Icon custom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Button from './components/button/Button'
 import Todo from './container/todo/todo'
 
 function App() {
