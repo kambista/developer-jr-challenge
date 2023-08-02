abffbc0c41542d366cb9c2ba7125a2e79141050d
Diego Bejar <diebejardelaguila@gmail.com>
2023-08-02 09:52:54 -0500

    config: Add cors url front docker

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ import {
     deleteTodoById,
 } from './database.js'
 
-const allowedOrigins = ['http://127.0.0.1:5173', 'http://localhost:5173'];
+const allowedOrigins = ['http://127.0.0.1:5173', 'http://localhost:5173','http://localhost:3015/'];
 
 
 const app = express();
