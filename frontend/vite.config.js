4e22064f73731e1a5bccc1120b6e3049edad1e39
Diego Bejar <diebejardelaguila@gmail.com>
2023-08-02 09:52:22 -0500

    config: Add dockerization apps

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+    host: '0.0.0.0',
+  }
 })
